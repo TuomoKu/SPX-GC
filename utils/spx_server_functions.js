TuomoKu/SPX-GC

commit 74475eb4b638e362a94c2cfb88498dc234367189

chore: fix no-unused-vars errors (and some eval errors)

utils/spx_server_functions.js
@@ -9,12 +9,8 @@ const wavplayer = require('node-wav-player');
 const PlayoutCCG = require('./playout_casparCG.js');
 const glob = require("glob");
 const ip = require('ip')
-const { rejects } = require('assert');
-const axios = require('axios')
 
 const port = global.config.general.port || 5000;
-        
-let Connected=true; // used we messaging service
 
 // Use crypto instead of bcrypt:
 let crypto;
@@ -172,7 +168,7 @@ module.exports = {
       // Get files from a given folder and return an array of files
       const directoryPath = path.normalize(FOLDER);
       let fileList=[];
-      fs.readdirSync(directoryPath).forEach((file, index) => {
+      fs.readdirSync(directoryPath).forEach((file) => {
         // console.log(index + ': ' + file + '(' + path.extname(file) + ')');
         let TARGETFILETYPE = EXTENSION.toUpperCase()
         let CURRENTFILETYPE = path.extname(file).toUpperCase();
@@ -304,10 +300,9 @@ module.exports = {
   lang: function (str) {
     try {
       const spxlangfile = global.config.general.langfile || 'english.json';
-      let langpath = path.join(process.cwd(), 'locales', spxlangfile);
-      var lang = require(langpath);
-      const json = 'lang.' + str;
-      return eval(json) || str;  
+      const langpath = path.join(process.cwd(), 'locales', spxlangfile);
+      const lang = require(langpath);
+      return lang[str] || str;
     } catch (error) {
       logger.error('ERROR in spx.lang (str: ' + str + '): ' + error);
       return str + " missing from " + global.spxlangfile;
@@ -326,7 +321,7 @@ GetFilesAndFolders: function (datafolder) {
     };
 
     if (fs.existsSync(datafolder)) {
-      fs.readdirSync(datafolder).forEach((file, index) => {
+      fs.readdirSync(datafolder).forEach((file) => {
         const curPath = path.join(datafolder, file);
         if (fs.lstatSync(curPath).isDirectory())
           { 
@@ -566,17 +561,6 @@ function lPad(nro,len,char){
 }
 
 
-function checkInternetConnection(icheck) {
-  require('dns').lookup('google.com',function(err) {
-      if (err && err.code == "ENOTFOUND") {
-        icheck(false);
-      } else {
-        icheck(true);
-      }
-    })
-  }
-
-
 function datarootSize () {
     // counts projects and playlists
     return new Promise(resolve => {
@@ -585,7 +569,7 @@ function datarootSize () {
         let projects = 0;
         let rundowns = 0;
         let filesArr = glob.sync(folderpath + "/**/*.json")
-        filesArr.forEach((file,index) => {
+        filesArr.forEach((file) => {
           if (file.includes('profile.json')) {projects = projects + 1}
         });
         rundowns = (filesArr.length - projects); 

routes/routes-api-v1.js
@@ -14,14 +14,8 @@
 
 var express = require("express");
 const router = express.Router();
-const path = require('path');
-const fs = require('fs');
-const moment = require('moment');
 const logger = require('../utils/logger');
 logger.debug('API-v1 route loading...');
-const spx = require('../utils/spx_server_functions.js');
-const xlsx = require('node-xlsx').default;
-const directoryPath = path.normalize(global.config.general.dataroot);
 
 // ROUTES -------------------------------------------------------------------------------------------
 router.get('/', function (req, res) {

routes/routes-api.js
@@ -30,9 +30,8 @@ router.get('/logger/', async (req, res) => {
   let source  = req.query.source
   let level   = req.query.level.toLowerCase()
   let channel = req.query.channel
-  let msg = '(api/logger, ' + channel + ', ' + source + '): ' + message
-  // console.log(msg);
-  eval('logger.' + level + '(msg)'); // nasty, eh?
+  let msg = '(api/logger, ' + channel + ', ' + source + '): ' + message;
+  logger[level].call(logger[level], msg);
   res.sendStatus(200)
 }); // files route ended
 
@@ -94,7 +93,7 @@ router.post('/readExcelData', async (req, res) => {
 
 
 
-router.post('/savefile/:filebasename', async (req, res) => {
+router.post('/savefile/:filebasename', async (req) => {
   // FIXME: Function not in use?
   spx.talk('Saving file ' + req.params.filebasename);
 

routes/routes-application.js
@@ -17,7 +17,6 @@ const spxAuth = require('../utils/spx_auth.js');
 const jsdom = require("jsdom"); // for scanning js within templates
 const { JSDOM } = jsdom;
 const cors = require('cors');
-const directoryPath = path.normalize(global.config.general.dataroot);
 
 // ROOT ROUTES ----------------------------------------------------------------------------------------------
 router.get('/', spxAuth.CheckLogin, cors(), spx.getNotificationsMiddleware, function (req, res) {
@@ -552,7 +551,7 @@ router.delete('/shows/:foldername', spxAuth.CheckLogin, async (req, res) => {
   let datafolder = path.join(global.config.general.dataroot, req.params.foldername);
   const deleteFolderRecursive = function(datafolder) {
     if (fs.existsSync(datafolder)) {
-      fs.readdirSync(datafolder).forEach((file, index) => {
+      fs.readdirSync(datafolder).forEach((file) => {
         const curPath = path.join(datafolder, file);
         logger.debug('Deleting ' + curPath);
         
@@ -688,8 +687,6 @@ router.post('/gc/:foldername/:filename/', spxAuth.CheckLogin, async (req, res) =
       // CLONE TEMPLATE ITEM ON RUNDOWN (without reloading page) ///////////////////////////////////////////////////////////////////////////
 
       let freshID = data.cloneEpoch;
-      var newItemData = "";
-      var duplicateData = "";
       var duplicated = false;
 
       logger.info('Cloning item [' + data.sourceEpoch + '] to [' + freshID + '] in file [' + data.listname + '].');
@@ -1064,7 +1061,7 @@ router.post('/gc/clearPlayouts', spxAuth.CheckLogin, async (req, res) => {
 
     global.showprofile = path.join(global.config.general.dataroot, req.body.foldername, 'profile.json');
     global.profileDataJSONobj = await GetJsonData(global.showprofile);
-    global.profileDataJSONobj.templates.forEach(function(template,index)
+    global.profileDataJSONobj.templates.forEach(function(template)
       {
         // Clear CasparCG's FIXME: This should move to playout_casparCG.js -library
         // anf FIXME: only send commands uniquely... For now: plaster all!
@@ -1273,7 +1270,8 @@ async function GetDataFiles(FOLDERstr) {
 
 
 
-
+// TODO: dead function?
+// eslint-disable-next-line no-unused-vars
 async function orgGetDataFiles(FOLDERstr) {
   // return a list of all json files in the dataroot folder
   logger.debug("Getting json files from " + FOLDERstr + "...");

routes/routes-casparcg.js
@@ -9,11 +9,7 @@ const path = require('path');
 const logger = require('../utils/logger');
 logger.debug('Caspar-route loading...');
 const spx = require('../utils/spx_server_functions.js');
-const open = require('open'); // open url in browser
 
-const ip = require('ip')
-const ipad = ip.address(); // my ip address
-const port = global.config.general.port || 5000;
 
 
 // ROUTES CCG -----------------------------------------------------------------------------------
@@ -140,10 +136,10 @@ router.get('/control/:data', (req, res) => {
   for (var i = 0; i < arr.length; i++) {
     var obj = arr[i];
     if (obj.name == data.profile) {
-      GFX_Teml = eval("obj.templates." + data.element + ".templatefile");
-      GFX_Serv = eval("obj.templates." + data.element + ".server");
-      GFX_Chan = eval("obj.templates." + data.element + ".channel");
-      GFX_Laye = eval("obj.templates." + data.element + ".layer");
+      GFX_Teml = obj.templates[data.element].templatefile;
+      GFX_Serv = obj.templates[data.element].server;
+      GFX_Chan = obj.templates[data.element].channel;
+      GFX_Laye = obj.templates[data.element].layer;
     }
   }
   logger.debug('CCG/Control - Profile ' + data.profile + "', Template: '" + GFX_Teml, "', CasparCG: " + GFX_Serv + ", " + GFX_Chan + ", " + GFX_Laye);
@@ -222,10 +218,10 @@ router.get('/controljson/:data', (req, res) => {
   for (var i = 0; i < arr.length; i++) {
     var obj = arr[i];
     if (obj.name == data.profile) {
-      GFX_Teml = eval("obj.templates." + data.element + ".templatefile");
-      GFX_Serv = eval("obj.templates." + data.element + ".server");
-      GFX_Chan = eval("obj.templates." + data.element + ".channel");
-      GFX_Laye = eval("obj.templates." + data.element + ".layer");
+      GFX_Teml = obj.templates[data.element].templatefile;
+      GFX_Serv = obj.templates[data.element].server;
+      GFX_Chan = obj.templates[data.element].channel;
+      GFX_Laye = obj.templates[data.element].layer;
       GFX_JSON = JSON.stringify(data.jsonData);
     }
   }
@@ -280,6 +276,7 @@ global.config.casparcg.servers.forEach((element,index) => {
   ServerData.push({ name: CurName, host: CurHost, port: CurPort });
 
   // next two lines creates a dynamic variable for this loop iteration
+  // TODO: but why?
   var CurCCG = CurName + "= undefined";
   eval(CurCCG);
   CurCCG = new net.Socket();

server.js
@@ -143,7 +143,7 @@ app.engine('handlebars', exphbs({
       let html="";
       let AvailableFormats=['json', 'xml'];
       
-      AvailableFormats.forEach((Option,i) => {
+      AvailableFormats.forEach((Option) => {
         let curFormat = Option.toLowerCase();
         let cfgFormat = currentDataformat.toLowerCase();
         let sel = "";
@@ -160,12 +160,12 @@ app.engine('handlebars', exphbs({
       let html="";
       let AvailableServers=['-'];
       if (global.config.casparcg){
-        global.config.casparcg.servers.forEach((element,i) => {
+        global.config.casparcg.servers.forEach((element) => {
           AvailableServers.push(element.name);
         });
       }
       
-      AvailableServers.forEach((serverOption,i) => {
+      AvailableServers.forEach((serverOption) => {
         let curSrv = serverOption.toUpperCase();
         let cfgSrv = currentServer.toUpperCase();
         let sel = "";
@@ -229,7 +229,7 @@ app.engine('handlebars', exphbs({
               html += '<tr><td width="60%">\n';
               html += '<select id="select' + index + '" class="wide btn ' + control.bgclass + '">\n';
               if (control.items) {
-                control.items.forEach((option,nro) => {
+                control.items.forEach((option) => {
                   html += '<option value="' + option.value + '">' + option.text + '</option>\n';
                 });
               }
@@ -336,7 +336,7 @@ app.engine('handlebars', exphbs({
           value:'bg_grey15'
         }
       ];
-      AvailableOptions.forEach((OptionItem,i) => {
+      AvailableOptions.forEach((OptionItem) => {
         let sel = "";
         if (OptionItem.value==currentValue){ sel="selected" }
         html += '<option value="' + OptionItem.value + '" ' + sel + '>' + OptionItem.text + '</option>';
@@ -456,7 +456,7 @@ app.engine('handlebars', exphbs({
         {value:'6000',     text:'In 6 seconds'},
         {value:'10000',    text:'In 10 seconds'}
       ];
-      AvailableOptions.forEach((outOption,i) => {
+      AvailableOptions.forEach((outOption) => {
         let sel = "";
         let CurVal = currentOut.toLowerCase();
         let CurOpt = outOption.value.toLowerCase();
@@ -473,7 +473,7 @@ app.engine('handlebars', exphbs({
       logger.debug('Generating webplayout options. This selection: ' + currentLayer + '.');
       let html="";
       let AvailableLayers=['-',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-      AvailableLayers.forEach((layerOption,i) => {
+      AvailableLayers.forEach((layerOption) => {
         let curSrv = layerOption || '-';
         let cfgSrv = currentLayer || '-';
         let sel = "";
@@ -626,7 +626,7 @@ app.engine('handlebars', exphbs({
       let html = '';
       let sel = '';
       let fileListData = spx.getJSONFileList('./locales/');
-      fileListData.files.forEach((element,i) => {
+      fileListData.files.forEach((element) => {
         sel = '';
         if (element.name == global.config.general.langfile)
           {
@@ -644,14 +644,13 @@ app.engine('handlebars', exphbs({
       let html = "";
       let sel = "";
       let fullFilePath = "";
-      let SERVER_URL = 'http://' + ip.address() + ':' + port;
       let assetPath = path.normalize(assetfolder || '');
       let selectedValue = value;
       // let fullPath = path.join(__dirname, 'ASSETS', assetPath); // failed in PKG version
       let fullPath = path.join(process.cwd(), 'ASSETS', assetPath); // fixes the PKG get excel file list bug
       let fileList = spx.getFileList(fullPath, extension);
       if (fileList) {
-        fileList.forEach((fileRef,index) => {
+        fileList.forEach((fileRef) => {
           fullFilePath = assetfolder + fileRef;
           if (assetfolder.substr(assetfolder.length - 1)!="/") {
             assetfolder = assetfolder + "/";
@@ -717,10 +716,6 @@ const ROUTEapp = require('./routes/routes-application.js');
 app.use('/', ROUTEapp);
 
 const ROUTEccg = require('./routes/routes-casparcg.js');
-const { prependListener } = require('process'); // const { prependListener } = require('process') // DIFF?
-const { request } = require('http')
-const { data } = require('./utils/logger.js')
-const { Z_VERSION_ERROR } = require('zlib')
 app.use('/CCG', ROUTEccg);
 
 process.on('uncaughtException', function(err) {
@@ -738,6 +733,7 @@ process.on('uncaughtException', function(err) {
 
 
 var server = app.listen(port, (err) => {
+  if(err) throw err;
 
   let splash = 'START-UP INFORMATION:\n\n' + 
   'SPX GC ................. Copyright 2020-2021 SmartPX <tuomo@smartpx.fi>\n' +

utils/playout_casparCG.js
@@ -2,9 +2,7 @@
 // ================== functions alphabetical order  ==================================================
 
 const logger = require('./logger.js');
-const fs = require('fs');
 const path = require('path')
-const moment = require('moment');
 const spx = require('../utils/spx_server_functions.js');
 
 module.exports = {
@@ -86,7 +84,7 @@ module.exports = {
     var DataStr = "";
     if (data.command == "ADD" || data.command == "UPDATE") {
         if (data.fields) {
-          data.fields.forEach((item,index) => {
+          data.fields.forEach((item) => {
             logger.debug('  DATA --> ' + item.field + ' : ' + item.value);
             if (DataType == 'xml'){
               // generate data in XML format
@@ -163,7 +161,6 @@ module.exports = {
     let GFX_Serv = data.playserver;
     let GFX_Chan = data.playchannel;
     let GFX_Laye = data.playlayer;
-    let GFX_File = data.relpath;
     let GFX_CCGf = data.relpathCCG;
     let GFX_OPTS = data.playoptions;
 

utils/playout_webplayer.js
@@ -1,11 +1,6 @@
 
 // ================== functions alphabetical order  ==================================================
 
-const logger = require('./logger.js');
-const fs = require('fs');
-const path = require('path')
-const moment = require('moment');
-
 
 module.exports = {
 
@@ -14,11 +9,10 @@ module.exports = {
     // We pass the data object to be processed by the web renderer
     // First dataformat sanity check.
     // THIS FORMATS JSON AND SENDS IT FORWARDS in all cases: play, stop, update...
-    let DataType = data.dataformat;
     let TEMPLATEDATA = [];
     // console.log('Format [' + DataType + '] data.fields coming in before emit', data.fields);
     if (data.fields) {
-        data.fields.forEach((item,index) => {
+        data.fields.forEach((item) => {
           let tempObj={};
           tempObj[item.field] = item.value;
           TEMPLATEDATA.push(tempObj);

commit 3fd6b75715c5cfadd6ed23c0b0f08923ec62c592

fix: Fix language lookups broken by 74475eb

utils/spx_server_functions.js
@@ -20,6 +20,10 @@ try {
   logger.warn('Crypto support is disabled!');
 }
 
+function getByPath(obj, dottedName) {
+  return dottedName.split('.').reduce((leaf,name) => leaf ? leaf[name] : null, obj);
+}
+
 
 module.exports = {
 
@@ -302,7 +306,7 @@ module.exports = {
       const langpath = path.join(process.cwd(), 'locales', spxlangfile);
       // eslint-disable-next-line import/no-dynamic-require
       const lang = require(langpath);
-      return lang[str] || str;
+      return getByPath(lang, str) || str;
     } catch (error) {
       logger.error('ERROR in spx.lang (str: ' + str + '): ' + error);
       return str + " missing from " + global.spxlangfile;