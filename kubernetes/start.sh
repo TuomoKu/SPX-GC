591565cde6bfe755da9fbde715bcee93294f6180
asera79 <asera79@gmail.com>
2024-03-28 21:49:41 +0200

    Fixes start.sh

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -z "$(ls -A ./ASSETS)" ]; then
+if [ -z "$(ls -A ./ASSETS/templates)" ]; then
   cp -R ./_ASSETS/* ./ASSETS
 fi
 
-if [ -z "$(ls -A ./DATAROOT)" ]; then
+if [ -z "$(ls -A ./DATAROOT/HelloWorld-project)" ]; then
   cp -R ./_DATAROOT/* ./DATAROOT
 fi
 
