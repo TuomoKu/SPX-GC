07bdf18fa2cde37564406de0e71cdbccbf5c44e8
asera79 <asera79@gmail.com>
2024-03-28 22:04:15 +0200

    Fix start.sh

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -z "$(ls -A ./ASSETS/templates)" ]; then
-  cp -R ./_ASSETS/* ./ASSETS
+  rm -rf ./ASSETS/* && cp -R ./_ASSETS/* ./ASSETS
 fi
 
 if [ -z "$(ls -A ./DATAROOT/HelloWorld-project)" ]; then
-  cp -R ./_DATAROOT/* ./DATAROOT
+  rm -rf ./DATAROOT/* && cp -R ./_DATAROOT/* ./DATAROOT
 fi
 
 node server.js
