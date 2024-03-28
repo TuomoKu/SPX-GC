eb8a93361ec0212a690d11aa87bcc2c228acb641
asera79 <asera79@gmail.com>
2024-03-28 17:47:43 +0200

    Fixes script

--- kubernetes/reset.sh
+++ kubernetes/reset.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Remove data folder
 rm -rf ./ASSETS/* && rm -rf ./DATAROOT/*
 
-# Copy the default assets
 cp -R ./_ASSETS/* ./ASSETS && cp -R ./_DATAROOT/* ./DATAROOT
--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# Check if the ASSETS folder is empty and copy the default assets
 if [ -z "$(ls -A ./ASSETS)" ]; then
   cp -R ./_ASSETS/* ./ASSETS
 fi
 
-# Check if the DATAROOT folder is empty and copy the default dataroot
 if [ -z "$(ls -A ./DATAROOT)" ]; then
   cp -R ./_DATAROOT/* ./DATAROOT
 fi
