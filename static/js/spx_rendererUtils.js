e2bfba826fcd7b44781dbdb828648c08fb9fc8ea
Tuomo Kulomaa <tuomo@softpix.io>
2026-01-05 15:29:02 +0200

    fixes rendererUtils embed bug

--- static/js/spx_rendererUtils.js
+++ static/js/spx_rendererUtils.js
@@ -4,30 +4,34 @@
 // if found loads the script and executes it.
 
 onload = function() {
-    // console.log('spx_rendererUtils.js onload');
-    let docParams = new URLSearchParams(document.location.search);
-    let parParams = new URLSearchParams(parent.location.search);
-    let topParams = new URLSearchParams(top.location.search);
-    let curParams = null;
+    try {
+        // console.log('spx_rendererUtils.js onload');
+        let docParams = new URLSearchParams(document.location.search);
+        let parParams = new URLSearchParams(parent.location.search);
+        let topParams = new URLSearchParams(top.location.search);
+        let curParams = null;
 
-    if (docParams.size>0) {
-        curParams = docParams;
-    } else if (parParams.size>0) {
-        curParams = parParams;
-    } else if (topParams.size>0) {
-        curParams = topParams;
-    } 
+        if (docParams.size>0) {
+            curParams = docParams;
+        } else if (parParams.size>0) {
+            curParams = parParams;
+        } else if (topParams.size>0) {
+            curParams = topParams;
+        } 
 
-    if (curParams && curParams.has('runScript')) {
-        let jsFile = '/ExtraFunctions/' + curParams.get('runScript') + '.js';
-        console.log('SPX rendererUtils ->', jsFile , '-> init()');
-        import(jsFile)
-            .then(module => {
-                module.init();
-            })
-            .catch(err => {
-                console.error('SPX Error - loading init script required by the SPX Renderer URL failed: ' + jsFile);
-             })
-        ;
+        if (curParams && curParams.has('runScript')) {
+            let jsFile = '/ExtraFunctions/' + curParams.get('runScript') + '.js';
+            console.log('SPX rendererUtils ->', jsFile , '-> init()');
+            import(jsFile)
+                .then(module => {
+                    module.init();
+                })
+                .catch(err => {
+                    console.error('SPX Error - loading init script required by the SPX Renderer URL failed: ' + jsFile);
+                })
+            ;
+        }
+    } catch (e) {
+        console.error('SPX Error - spx_rendererUtils.js onload() exception: ' + e.message);
     }
 }
