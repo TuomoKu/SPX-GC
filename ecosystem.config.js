65043c585229da1e19c243866780ed78f3b26630
iamflorencejay <florencejaymunar@proton.me>
2026-03-02 20:20:58 +0800

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-  apps : [
-    {
-    'name': 'GC1',
-    'script': 'server.js',
-    'args': 'config.json'
-    },
-    {
-      'name': 'GC2',
-      'script': 'server.js',
-      'args': 'config2.json'
-      }
-  ]
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
 };
